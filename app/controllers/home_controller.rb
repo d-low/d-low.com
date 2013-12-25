a4a1bcedebd06c85fd8a15ea7d89256ec192cb78
Mike DiLorenzo <mike.dilorenzo@gmail.com>
2013-12-25 14:47:57 -0700

    Added content for home page.  Styles are behavior are next...

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
 
   def index
     @home_model = Home.new()
+    @random_image = @home_model.newest_post.get_random_image
 
     respond_to do |format|
       format.html # views/home/index.html.haml
