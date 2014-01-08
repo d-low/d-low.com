42f6227f613ece4d0076ee8bc82936b3d4879f8c
Mike DiLorenzo <mike.dilorenzo@gmail.com>
2014-01-07 22:24:16 -0700

    Added contents navigation to bottom of content pages.

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -2,8 +2,9 @@ class ContentController < ApplicationController
   attr_reader :content
   
   def index
-    path = params[:path]
-    @content = Content.new(path)
+    @home_model = Home.new()
+    @random_image = @home_model.newest_post.get_random_image
+    @content = Content.new(params[:path])
 
     respond_to do |format|
       format.html # views/content/index.html.haml
