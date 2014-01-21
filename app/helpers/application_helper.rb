3a71c4abe05040b0afc77599a9626609f50afb43
Mike DiLorenzo <mike.dilorenzo@gmail.com>
2014-01-20 21:55:35 -0700

    Added javascript anchor href helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def number_to_ordinal number 
     I18n.with_locale(:en) { number.to_words ordinal: true }
   end
+
+  def javascript_anchor_href
+    'javascript:void(0);'
+  end
 end
