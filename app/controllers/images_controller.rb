84b77a74e6e5258d5c6b5222ac5a992e02acda31
Mike DiLorenzo <mike.dilorenzo@gmail.com>
2014-05-27 23:18:29 -0600

    Enable x-sendfile header so Apache can serve images outside asset pipeline.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,6 +2,13 @@ class ImagesController < ApplicationController
   def show
     file = params[:file]
 
+    # NOTE: There is no need to sanitize the file and ensure that it represents
+    # a valid image file becuase there is no way to get to this controller
+    # method unless the path begins with "/images" so a malicious user cannot
+    # download any file from our site, only those requests that begin with 
+    # "/iamges" make it here.
+
     send_file File.join(Home::CONTENT_ROOT_DIRECTORY, file), :disposition => 'inline'
+    end
   end 
 end
